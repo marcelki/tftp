8a34462a221d156bda415983343655c46d281b9a
Marcel Kirschner <m.kirschnervfx@gmail.com>
2016-08-11 11:04:11 +0200

    Remove unnecessary 'Convert' function.

--- netascii/netascii.go
+++ netascii/netascii.go
@@ -16,12 +16,6 @@ func WriteTo(b []byte, bw *bufio.Writer) (n int, err error) {
 	return n, nil
 }
 
-func Convert(b []byte) []byte {
-	b = bytes.Replace(b, []byte{'\r', 0}, []byte{'\r'}, -1)
-	b = bytes.Replace(b, []byte{'\r', '\n'}, []byte{'\n'}, -1)
-	return b
-}
-
 func ReadFull(r io.Reader, buf []byte) (n int, err error) {
 	if len(buf) == 0 {
 		return 0, io.ErrShortBuffer
